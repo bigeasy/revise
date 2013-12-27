c136339b7b5e818547a75cc432b73b02a3803f04
Alan Gutierrez <alan@prettyrobots.com>
2013-12-26 20:29:12 -0800

    Import utilities from MVCC.
    Import `extractor` and `comparator` function builders from MVCC.
    
    Closes #8.
    Closes #7.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-module.exports = 1
+exports.extractor = function (extractor) {
+    return function (record) {
+        return {
+            value: extractor(record),
+            version: record.version
+        }
+    }
+}
+
+exports.comparator = function (comparator) {
+    return function (a, b) {
+        var compare = comparator(a.value, b.value)
+        if (compare) return compare
+        return a.version - b.version
+    }
+}
